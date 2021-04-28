e659902a3e9c45bfee7756a29fb0c795d4e65a75
Jaime Bastos-mendes armijo <jaimebastos-mendesarmijo@MacBook-Air-de-Jaime.local>
2021-04-28 12:11:08 +0200

    styled

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-// Endpoints
-router.get('/', (req, res) => res.render('pages/index'))
+const Beach = require('./../models/beach.model')
 
+// Endpoints
+// router.get('/', (req, res) => res.render('pages/index'))
+
+router.get('/', (req, res) => {
+
+    Beach
+        .find()
+        .select("image name")
+        .then(allImages => res.render('pages/index', { allImages }))
+        .catch(err => console.log(err))
+})
 
 module.exports = router
