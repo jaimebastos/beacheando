601dea14139b6eed214b41221ebd0d83748910c0
Jaime Bastos-mendes armijo <jaimebastos-mendesarmijo@MacBook-Air-de-Jaime.local>
2021-04-28 19:18:52 +0200

    saved

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose')
 module.exports = {
     cleanText: text => text.trim(),
     capitalizeText: text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
-    isAdmin: user => user && user.role === 'ADMIN',
-
+    isAdmin: user => user && user.role === 'ADMIN'
 }
